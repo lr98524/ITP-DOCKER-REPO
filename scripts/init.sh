4901bc636b75c67c562fdd71c5bad6cc995cc187
Liam <lr98524@email.vccs.edu>
2025-02-20 23:22:23 -0500

    Even more debugging, YAYgit add scripts/init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,10 +8,10 @@ rm -rf volumes/final-project/{config,html}/*
 docker pull nginx:alpine3.21
 docker run --rm --name temp-nginx -d nginx:alpine3.21
 
-docker cp temp-nginx:/etc/nginx.conf.d volumes/final-project/config
+docker cp temp-nginx:/etc/nginx/conf.d volumes/final-project/config
 docker cp temp-nginx:/etc/nginx/nginx.conf volumes/final-project/config/nginx.conf
 
-docker cp temp-nginx:/etc/nginx.conf.d volumes/home-page/config
+docker cp temp-nginx:/etc/nginx/conf.d volumes/home-page/config
 docker cp temp-nginx:/etc/nginx/nginx.conf volumes/home-page/config/nginx.conf
 
 docker cp temp-nginx:/usr/share/nginx/html volumes/home-page/
